6b7ea1cc14e330d74bbde7e7a827f03e0ac96b3f
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-08-19 20:44:54 -0300

    fix: corrige o nome do app para 'itens' e atualiza a URL para a lista de itens

--- apps/item/urls.py
+++ apps/item/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import ItemList, ItemDetail
 
 
-app_name = 'item'
+app_name = 'itens'
 
 urlpatterns = [
-    path('', ItemList.as_view(), name='item-list-create'),
+    path('', ItemList.as_view(), name='item-list'),
     path('<int:pk>/', ItemDetail.as_view(), name='item-detail'),
 ]
--- apps/item/views.py
+++ apps/item/views.py
@@ -5,10 +5,12 @@ from .serializers import ItemSerializer
 
 
 class ItemList(generics.ListCreateAPIView):
+    """View para listar e criar itens"""
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
 
 class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    """View para obter, atualizar ou deletar um item"""
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
