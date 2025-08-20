23d89cf6d6fc9282a47cbccc8df68c49b7c44dc8
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-08-19 21:18:35 -0300

    feat: adiciona inline de Pedido no admin de usuário e adiciona pedido na pág. de admin

--- apps/usuario/admin.py
+++ apps/usuario/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from apps.pedido.models import Pedido
+
+
+class PedidoInline(admin.TabularInline):
+    model = Pedido
+    fields = ['id', 'itens']
+    extra = 0
+    show_change_link = True
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'first_name',
+        'last_name',
+        'username',
+        'email'
+    ]
+    inlines = [PedidoInline]
+
+
+admin.site.unregister(User)  # Retira o registro de User padrão
+admin.site.register(User, UserAdmin)
